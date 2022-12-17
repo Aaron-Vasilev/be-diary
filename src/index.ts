b3b6f89d5c3f3cfa7b7bfe358d8c191f8d0ce2a4
Aaron Vasilev <oigen711@gmail.com>
2022-12-17 21:30:21 +0200

    Added default route

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ import { RegisterAuthRoute } from './routes/auth'
 import { JWTValidation } from './controller/auth'
 
 const server = Fastify({
-  logger: Boolean(process.env.NODE_ENV === "dev"),
+  logger: Boolean(process.env.NODE_ENV === 'dev'),
 }).withTypeProvider<TypeBoxTypeProvider>()
 
 const port = +process.env.PORT || 8080
-
+const host = process.env.NODE_ENV === 'dev' ? 'localhost' : '0.0.0.0'
 server.register(postgres, {
   connectionString: process.env.DATABASE_URL
 })
@@ -22,10 +22,17 @@ RegisterAuthRoute(server, {})
 RegisterQuestionRoute(server, {})
 RegisterNoteRoute(server, {})
 
-server.listen({ port }, (err, address) => {
+server.listen({ port, host }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
   console.log(`Server listening at ${address}`)
+})
+
+server.get('/', {}, (request, reply) => {
+  reply
+    .code(200)
+    .type('text/html')
+    .send("<h1>Sup</h1>")
 })
