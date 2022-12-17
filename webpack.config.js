c72153a2a73baa79333b3b816339dabba18fee95
Aaron Vasilev <oigen711@gmail.com>
2022-12-17 20:25:03 +0200

    Added postinstall command

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   externals: [nodeExternals()], // removes node_modules from your final bundle
   entry: './src/index.ts', // make sure this matches the main root of your code
   output: {
-    path: path.join(__dirname, 'bundle'), // this can be any path and directory you want
+    path: path.join(__dirname, 'dist'), // this can be any path and directory you want
     filename: 'app.js',
   },
   resolve: {
